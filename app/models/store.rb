66f51499798c56e8b962b02ffabea27754768c05
BFunk <bret.e.funk@gmail.com>
2017-12-13 16:50:39 -0500

    api and view working, need to refactor

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,12 @@
 class Store < ApplicationRecord
+  attr_reader :store_id, :store_type, :name, :address, :city, :distance
+
+  def initialize(attrs)
+    @store_id = attrs[:storeId]
+    @store_type = attrs[:storeType]
+    @name = attrs[:name]
+    @address = attrs[:address]
+    @city = attrs[:city]
+    @distance = attrs[:distance]
+  end
 end
